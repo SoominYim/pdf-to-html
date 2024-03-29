27b1cbface14afc6ca578a08a47aaf923031c45f
SoominYim <soo15soo@kakao.com>
2024-03-29 13:55:00 +0900

    feat : PDF TO HTML

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <ul class="tool-bar">
+      <li>
+        <input type="file" accept=".pdf" @change="changeFile" />
+      </li>
+      <li>
+        <button @click="page = page > 1 ? page - 1 : page">Prev</button>
+        <span>{{ page }} / {{ pages }}</span>
+        <button @click="page = page < pages ? page + 1 : page">Next</button>
+      </li>
+      <li>
+        <button @click="scale = scale > 0.25 ? scale - 0.25 : scale">-</button>
+        <span>{{ scale * 100 }}%</span>
+        <button @click="scale = scale < 2 ? scale + 0.25 : scale">+</button>
+      </li>
+    </ul>
+    <div class="content">
+      <VuePDF :pdf="pdf" :page="page" :scale="scale" text-layer="" />
+    </div>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref } from "vue";
+import { VuePDF, usePDF } from "@tato30/vue-pdf";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+const file = ref(null);
+const { pdf, pages } = usePDF(file);
+
+const scale = ref(1);
+const page = ref(1);
+
+function changeFile(event) {
+  const selectedFile = event.target.files[0];
+  if (selectedFile) {
+    file.value = URL.createObjectURL(selectedFile);
   }
 }
 </script>
 
-<style>
+<style scoped></style>
+
+<style lang="css">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
+.tool-bar {
+  display: flex;
+  justify-content: center;
+  list-style-type: none;
+  gap: 10px;
+}
+.content {
+  width: fit-content;
+  height: auto;
+  margin: 0 auto;
+}
+@import "./style/textLayer.css";
+@import "./style/annotationLayer.css";
 </style>
